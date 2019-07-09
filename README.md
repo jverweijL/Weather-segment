8680b212b394d3b8973d3424119beda52ce62bcf
Jan Verweij <jan.verweij@liferay.com>
2019-07-09 21:47:41 +0200

    rewrite some code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Based on the weather you can show different content.
 
-show a short skirt vs. a rainjacket
+show a skirt vs. a rainjacket
 show a convertible vs. a 4x4 in the snow
 
--- src/main/java/com/liferay/demo/segments/context/contributor/WeatherRequestContextContributor.java
+++ src/main/java/com/liferay/demo/segments/context/contributor/WeatherRequestContextContributor.java
@@ -14,27 +14,28 @@
 
 package com.liferay.demo.segments.context.contributor;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import com.liferay.portal.kernel.json.JSONException;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.segments.context.Context;
 import com.liferay.segments.context.contributor.RequestContextContributor;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientHandlerException;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.UniformInterfaceException;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.core.impl.provider.entity.StringProvider;
 
 import javax.servlet.http.HttpServletRequest;
 
-
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.osgi.service.component.annotations.Component;
 
+import java.io.IOException;
+
 /**
- * @author Kris
+ * @author Kris / Jan
  */
 @Component(
 	immediate = true,
@@ -52,110 +53,80 @@ public class WeatherRequestContextContributor
 	@Override
 	public void contribute(
 		Context context, HttpServletRequest httpServletRequest) {
-		
-		boolean hardCodeIt = true;
-		
-		if(hardCodeIt == true)
+
+		String actualWeather = "unknown";
+
+		if(context.get(KEY) == null)
 		{
-			context.put(KEY, "sunny");
-		}
-		else {
-		
-						if(context.get(KEY) == null)
-						{		
-							//String ipAddress = httpServletRequest.getRemoteAddr();
-							// Hardcoded to UK office IP Address
-							String ipAddress = "92.234.68.98";
-							String actualWeather = "";
-							DefaultClientConfig defaultClientConfig = new DefaultClientConfig();
-							defaultClientConfig.getClasses().add(JacksonJsonProvider.class);
-							//You need to add this config due to OSGI craziness
-							defaultClientConfig.getClasses().add(StringProvider.class);
-							Client client = Client.create(defaultClientConfig);
-						    WebResource webResource = client.resource("https://ipapi.co/" + ipAddress + "/json");
-						    
-						    ClientResponse response;
-							try {
-								response = webResource.get(ClientResponse.class);
-								if (response.getStatus() == 200)
-							    {			    
-							    		
-							    //parse JSON objects
-							    
-									    JSONObject jsonObject;
-										try {
-											jsonObject = JSONFactoryUtil.createJSONObject(response.getEntity(String.class));
-											Object latitude = jsonObject.get("latitude");
-										    Object longitude = jsonObject.get("longitude");			
-										    
-										    //Call weather API
-										    
-										    String apiKey = "1923b9fbe9106354dd670445a384b48d";
-										    Client weatherClient = Client.create(defaultClientConfig);
-										    WebResource weatherWebResource = weatherClient.resource("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=" + apiKey);
-										    ClientResponse weatherResponse = weatherWebResource.get(ClientResponse.class);
-										    
-										    if(weatherResponse.getStatus() == 200) {
-										    	
-												    JSONObject weatherJsonObject = JSONFactoryUtil.createJSONObject(weatherResponse.getEntity(String.class));
-												    int weatherCode = weatherJsonObject.getJSONArray("weather").getJSONObject(0).getInt("id");
-												    if ((weatherCode >= 200 && weatherCode < 600) || weatherCode == 900 || weatherCode == 901 || weatherCode == 902) {
-												    	actualWeather="rainy";
-												    }
-												    else
-												    	if (weatherCode >= 600 && weatherCode <= 622) {
-													    	actualWeather="snowy";
-													    }
-												    	else
-												    		if (weatherCode == 800 || weatherCode == 801 ) {
-												    			actualWeather="sunny";
-												    		}
-												    		else
-												    			if (weatherCode == 802 || weatherCode == 803|| weatherCode == 804) {
-												    				actualWeather="cloudy";
-												    			}
-												    			else
-												    				if (weatherCode == 905 || weatherCode >= 956 ) {
-												    					actualWeather="windy";
-												    				}
-												    				else
-												    					if (weatherCode == 701 || weatherCode == 721 || weatherCode == 771 || weatherCode == 741) {
-												    						actualWeather="misty";
-												    					}
-												    						
-												    System.out.println("The weather rule is running and the weather is " + actualWeather);
-												    context.put(KEY, actualWeather);
-										    }
-										    else {
-										    	context.put(KEY, "unknown");
-										    }
-											
-											
-										} catch (JSONException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										} catch (ClientHandlerException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										} catch (UniformInterfaceException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										}
-										
-							    }
-							    else {
-							    	context.put(KEY, "unknown");
-							    
-							    }
-							} catch (UniformInterfaceException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							} catch (ClientHandlerException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							} 
-						    
-						}
+			try {
+				ObjectMapper mapper = new ObjectMapper();
+
+				String ipAddress = httpServletRequest.getRemoteAddr();
+				_log.debug(String.format("Using ip %s", ipAddress));
+
+				HttpUriRequest request = RequestBuilder
+						.get("https://ipapi.co/" + ipAddress + "/json")
+						.build();
+
+				JsonNode jsonResult = mapper.readTree(getRequest(request));
+
+				Object latitude = jsonResult.get("latitude");
+				Object longitude = jsonResult.get("longitude");
+
+				_log.debug(String.format("Looking up weather at lat %s and lon %s", latitude, longitude));
+
+				//Call weather API
+				//TODO make this configurable... in properties
+				String apiKey = "1923b9fbe9106354dd670445a384b48d";
+
+				request = RequestBuilder
+						.get("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&APPID=" + apiKey)
+						.build();
+
+				jsonResult = mapper.readTree(getRequest(request));
+
+				int weatherCode = jsonResult.withArray("weather").get(0).get("id").asInt(0);
+
+				if ((weatherCode >= 200 && weatherCode < 600) || weatherCode == 900 || weatherCode == 901 || weatherCode == 902) {
+					actualWeather = "rainy";
+				} else if (weatherCode >= 600 && weatherCode <= 622) {
+					actualWeather = "snowy";
+				} else if (weatherCode == 800 || weatherCode == 801) {
+					actualWeather = "sunny";
+				} else if (weatherCode == 802 || weatherCode == 803 || weatherCode == 804) {
+					actualWeather = "cloudy";
+				} else if (weatherCode == 905 || weatherCode >= 956) {
+					actualWeather = "windy";
+				} else if (weatherCode == 701 || weatherCode == 721 || weatherCode == 771 || weatherCode == 741) {
+					actualWeather = "misty";
+				}
+
+			} catch (IOException e) {
+				_log.error(e.getMessage());
+				_log.debug(e.getStackTrace());
+			}
+
+			_log.debug(String.format("The weather rule is running and the weather is %s", actualWeather));
+			context.put(KEY, actualWeather);
 		}
 	}
+
+	private String getRequest(HttpUriRequest request) {
+		HttpClient client = HttpClientBuilder.create().build();
+
+		HttpResponse response = null;
+		try {
+			response = client.execute(request);
+			HttpEntity entity = response.getEntity();
+			if (response.getStatusLine().getStatusCode() == 200) {
+				return EntityUtils.toString(entity, "UTF-8");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return "";
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(WeatherRequestContextContributor.class);
 }
